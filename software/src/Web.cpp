9ff35a3c464a0c6a546d77b2204117df1babf8ef
felixslama <firejava@protonmail.com>
2022-03-27 17:37:29 +0200

    ota page working, functionality to be implemented

--- software/src/Web.cpp
+++ software/src/Web.cpp
@@ -66,6 +66,7 @@ HTTPSServer secureServer = HTTPSServer(&cert);
 #endif
 // Declare some handler functions for the various URLs on the server
 void handleRoot(HTTPRequest * req, HTTPResponse * res);
+void handleUpdate(HTTPRequest * req, HTTPResponse * res);
 void handle404(HTTPRequest * req, HTTPResponse * res);
 void serverTask(void *params);
 void initHTTPS(){
@@ -103,9 +104,11 @@ void serverTask(void *params){
   
   ResourceNode * nodeRoot    = new ResourceNode("/", "GET", &handleRoot);
   ResourceNode * node404     = new ResourceNode("", "GET", &handle404);
+  ResourceNode * nodeUpdate  = new ResourceNode("/update", "GET",&handleUpdate);
   
   // Add the root node to the server
   secureServer.registerNode(nodeRoot);
+  secureServer.registerNode(nodeUpdate);
   // Add the 404 not found node to the server.
   secureServer.setDefaultNode(node404);
   
@@ -127,17 +130,10 @@ void handleRoot(HTTPRequest * req, HTTPResponse * res) {
 
   // The response implements the Print interface, so you can use it just like
   // you would write to Serial etc.
-  res->println("<!DOCTYPE html>");
-  res->println("<html>");
-  res->println("<head><title>Hello World!</title></head>");
-  res->println("<body>");
-  res->println("<h1>Hello World!</h1>");
-  res->print("<p>Your server is running for ");
-  // A bit of dynamic data: Show the uptime
-  res->print((int)(millis()/1000), DEC);
-  res->println(" seconds.</p>");
-  res->println("</body>");
-  res->println("</html>");
+  res->println(aqmIndex);
+}
+void handleUpdate(HTTPRequest * req, HTTPResponse * res) {
+  res->println(updateIndex);
 }
 
 void handle404(HTTPRequest * req, HTTPResponse * res) {
@@ -158,4 +154,6 @@ void handle404(HTTPRequest * req, HTTPResponse * res) {
   res->println("<head><title>Not Found</title></head>");
   res->println("<body><h1>404 Not Found</h1><p>The requested resource was not found on this server.</p></body>");
   res->println("</html>");
+
+  
 }
