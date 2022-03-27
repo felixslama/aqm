3e99affe0f8fe3cb421fa3d0e6135d8e1cfde4ff
felixslama <firejava@protonmail.com>
2022-03-27 17:21:58 +0200

    HTTPS async now working fine, OTA to be implimented

--- software/src/Web.cpp
+++ software/src/Web.cpp
@@ -50,8 +50,15 @@ void initWeb(){
 }
 
 using namespace httpsserver;
-SSLCert * cert;
-HTTPSServer * secureServer;
+
+#include "cert.h"
+#include "private_key.h"
+
+SSLCert cert = SSLCert(
+  example_crt_DER, example_crt_DER_len,
+  example_key_DER, example_key_DER_len
+);
+HTTPSServer secureServer = HTTPSServer(&cert);
 #if CONFIG_FREERTOS_UNICORE
 #define ARDUINO_RUNNING_CORE 0
 #else
@@ -67,35 +74,48 @@ void initHTTPS(){
 }
 
 void serverTask(void *params){
+  Serial.println("woah 1");
+  /*
   cert = new SSLCert();
+  Serial.println("woah 2");
+  
   int createCertResult = createSelfSignedCert(
     *cert,
     KEYSIZE_2048,
     "CN=myesp32.local,O=FancyCompany,C=DE",
     "20190101000000",
     "20300101000000"
-  );
+  ); 
+  SSLCert startCert = SSLCert(
+    cert->getCertData(),cert->getCertLength(),
+    cert->getPKData(),cert->getPKLength()
+  ); */
+  Serial.println("woah 3");
+  /*
   if (createCertResult != 0) {
     Serial.printf("Cerating certificate failed. Error Code = 0x%02X, check SSLCert.hpp for details", createCertResult);
     return;
-  }
+  }else{
+    Serial.println("woah good cert");
+  }*/
+  
   Serial.println("Creating the certificate was successful");
-  secureServer = new HTTPSServer(cert);
+  
   ResourceNode * nodeRoot    = new ResourceNode("/", "GET", &handleRoot);
   ResourceNode * node404     = new ResourceNode("", "GET", &handle404);
-
+  
   // Add the root node to the server
-  secureServer->registerNode(nodeRoot);
+  secureServer.registerNode(nodeRoot);
   // Add the 404 not found node to the server.
-  secureServer->setDefaultNode(node404);
-
+  secureServer.setDefaultNode(node404);
+  
   Serial.println("Starting server...");
-  secureServer->start();
-  if (secureServer->isRunning()) {
+  secureServer.start();
+  Serial.println("woah after start");
+  if (secureServer.isRunning()) {
     Serial.println("Server ready.");
     while(true){
-      secureServer->loop();
-      Serial.println("woah");
+      secureServer.loop();
     }
   }
 }
