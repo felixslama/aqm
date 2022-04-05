23dd18d1bc655242ca62391709bc57f86ea39c56
K1llf0rce <fteppan@protonmail.com>
2022-04-05 20:27:46 +0200

    add first case design

--- software/createcert.sh
+++ software/createcert.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 set -e
+
+procSpinner() {
+    spin='◢◣◤◥'                                                                                                                                                                                                                                                                  
+    i=0                                                                                                                                       
+    while kill -0 $1 2>/dev/null                                                                                                            
+    do                                                                                                                                        
+    i=$(( (i+1) %4 ))                                                                                                                       
+    printf "\r${spin:$i:1}"                                                                                                                 
+    sleep 0.08                                                                                                                              
+    done
+}
+
 rm -f AQMca.* AQM.* cert.h key.h
 
-openssl genrsa -out AQMca.key 4096
+openssl genrsa -out AQMca.key 4096 2> /dev/null &
+procSpinner $!
+
 cat > AQMca.conf << EOF  
 [ req ]
 distinguished_name     = req_distinguished_name
@@ -14,10 +28,11 @@ L = Villach
 O = AETHERENGINEERING
 CN = aether.local
 EOF
-openssl req -new -x509 -days 3650 -key AQMca.key -out AQMca.crt -config AQMca.conf
+
+openssl req -new -x509 -days 3650 -key AQMca.key -out AQMca.crt -config AQMca.conf  2> /dev/null
 echo "01" > AQMca.srl
-openssl genrsa -out AQM.key 4096
-# create certificate signing request
+openssl genrsa -out AQM.key 4096 2> /dev/null
+
 cat > AQM.conf << EOF  
 [ req ]
 distinguished_name     = req_distinguished_name
@@ -29,6 +44,7 @@ L = Villach
 O = AETHERENGINEERING
 CN = aether.local
 EOF
+
 openssl req -new -key AQM.key -out AQM.csr -config AQM.conf
 openssl x509 -days 3650 -CA AQMca.crt -CAkey AQMca.key -in AQM.csr -req -out AQM.crt
 openssl verify -CAfile AQMca.crt AQM.crt
